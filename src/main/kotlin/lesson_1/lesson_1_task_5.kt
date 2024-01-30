c5f98ed2cd5609461811b17869612e0ea6a7464b
romanmacnev <machnevrv@tech.bcs.ru>
2024-01-31 00:57:48 +0300

    KS-1-5 Перевод из секунд в часы и минуты

---
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    var seconds = "6480"
+    val hours = "01"
+    val minutes = "48"
+    seconds = "00"
+
+    print(hours)
+    print(":")
+    print(minutes)
+    print(":")
+    print(seconds)
+}
