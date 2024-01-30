9dda85bcb9c39df40d51f896c87cf6fdb53e7988
romanmacnev <machnevrv@tech.bcs.ru>
2024-01-31 01:08:08 +0300

    KS-2-1 Средний балл класса

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val firstPeople = 3
+    val secondPeople = 4
+    val thirdPeople = 3
+    val fourthPeople = 5
+    val middleBall = (firstPeople + secondPeople + thirdPeople + fourthPeople) / 4f
+
+    println(middleBall)
+}
