341aa7f532a609ebd5a0ae78f5b42e3650e5d609
romanmacnev <machnevrv@tech.bcs.ru>
2024-02-14 00:44:09 +0300

    KS-3-1 Вывод приветствий

---
+++ src/main/kotlin/lessons_3/lessons_3_task_1.kt
@@ -0,0 +1,12 @@
+package org.example.lessons_3
+
+fun main() {
+    var greeting = "Добрый день"
+    val userName = "Роман"
+
+    println("$greeting, $userName")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName")
+}
