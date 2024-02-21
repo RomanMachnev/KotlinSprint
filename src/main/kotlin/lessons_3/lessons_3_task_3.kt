873385859ea54a54efe1a45d3c1b220a01396d1a
romanmacnev <machnevrv@tech.bcs.ru>
2024-02-21 16:16:52 +0300

    KS-3-3 Изменил строку на число в переменной

--- src/main/kotlin/lessons_3/lessons_3_task_3.kt
+++ src/main/kotlin/lessons_3/lessons_3_task_3.kt
@@ -1,19 +1,19 @@
 package org.example.lessons_3
 
 fun main() {
-    val mainNumber = "9"
+    val mainNumber = 9
 
     println(
         """
-        $mainNumber x 1 = ${mainNumber.toInt() * 1}
-        $mainNumber x 2 = ${mainNumber.toInt() * 2}
-        $mainNumber x 3 = ${mainNumber.toInt() * 3}
-        $mainNumber x 4 = ${mainNumber.toInt() * 4}
-        $mainNumber x 5 = ${mainNumber.toInt() * 5}
-        $mainNumber x 6 = ${mainNumber.toInt() * 6}
-        $mainNumber x 7 = ${mainNumber.toInt() * 7}
-        $mainNumber x 8 = ${mainNumber.toInt() * 8}
-        $mainNumber x 9 = ${mainNumber.toInt() * 9}
+        $mainNumber x 1 = ${mainNumber * 1}
+        $mainNumber x 2 = ${mainNumber * 2}
+        $mainNumber x 3 = ${mainNumber * 3}
+        $mainNumber x 4 = ${mainNumber * 4}
+        $mainNumber x 5 = ${mainNumber * 5}
+        $mainNumber x 6 = ${mainNumber * 6}
+        $mainNumber x 7 = ${mainNumber * 7}
+        $mainNumber x 8 = ${mainNumber * 8}
+        $mainNumber x 9 = ${mainNumber * 9}
     """.trimIndent()
     )
 }
